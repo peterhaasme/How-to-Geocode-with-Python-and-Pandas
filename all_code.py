e651d8f18bb7c850548548b1d72356a1916f79e2
peterhaasme <51498910+peterhaasme@users.noreply.github.com>
2020-01-14 17:20:21 -0500

    Update all_code.py

--- all_code.py
+++ all_code.py
@@ -1,4 +1,6 @@
 !pip install geopy
+!pip install folium
+import folium
 
 from geopy.geocoders import Nominatim
 from geopy import distance
@@ -21,3 +23,16 @@ print(distance_calc_mi)
 # distance in kilometers
 distance_calc_km = distance.distance(location_1_gps, location_2_gps).km
 print(distance_calc_km)
+
+# Geocode in a pandas DataFrame
+df = pd.read_csv('sample_addresses.csv')
+df['full_address'] = df.address + "," + df.city + "," + df.state
+df['gcode'] = df.full_address.apply(geolocator.geocode)
+df['lat'] = [g.latitude for g in df.gcode]
+df['long'] = [g.longitude for g in df.gcode]
+
+# Map the addresses
+mapa = folium.Map(location=(36.104087829589844,-86.77576446533203), zoom_start=5)
+for index, row in df.iterrows():
+  folium.Marker(location=(row['lat'],row['long'])).add_to(mapa)
+display(mapa)
